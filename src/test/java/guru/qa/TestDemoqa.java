35a927226aded4ffdae0a1593f4628040b9931d5
Артемий <rick-glat@mail.ru>
2021-10-29 17:18:50 +0300

    added TestDemoqa.java

--- src/test/java/guru/qa/TestDemoqa.java
+++ src/test/java/guru/qa/TestDemoqa.java
@@ -1,12 +1,16 @@
 package guru.qa;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.Text;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestReporter;
+import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestDemoqa {
     @BeforeAll
@@ -21,11 +25,15 @@ public class TestDemoqa {
         $("#firstName").setValue("1Name");
         $("#lastName").setValue("2LastName");
         $("#userEmail").setValue("qwe@qas.ru");
-        $("#gender-radio-3").setValue("Other");
-        $("userNumber").setValue("889987632");
-        $("dateOfBirthInput").click();
-        $("#uploadPicture").uploadFromClasspath("лицо моё");
-        $("").setValue("");
-        $("").setValue("");
+        $$(".custom-radio").get(1).click();
+        $("#userNumber").setValue("12345678910");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("2020");
+        $$(".react-datepicker__day").find(Condition.text("12")).click();
+        $("#subjectsInput").setValue("Physics").pressEnter();
+        $(byText("Music")).click();
+        $(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("facemy.jpg");
     }
 }
