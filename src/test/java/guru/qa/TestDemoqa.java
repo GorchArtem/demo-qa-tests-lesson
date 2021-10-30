f9692055e4ac39ec70c734eb6777035d931aed3f
Артемий <rick-glat@mail.ru>
2021-10-30 23:00:32 +0300

    added .gitignore

--- src/test/java/guru/qa/TestDemoqa.java
+++ src/test/java/guru/qa/TestDemoqa.java
@@ -9,31 +9,42 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestReporter;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestDemoqa {
     @BeforeAll
-    static void beforeAll(){
+    static void beforeAll() {
         Configuration.startMaximized = true;
     }
 
     @Test
-    void fillFormTest(){
+    void fillFormTest() {
         open("https://demoqa.com/automation-practice-form");
 
         $("#firstName").setValue("1Name");
         $("#lastName").setValue("2LastName");
         $("#userEmail").setValue("qwe@qas.ru");
         $$(".custom-radio").get(1).click();
-        $("#userNumber").setValue("12345678910");
+        $("#userNumber").setValue("1234567891");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("January");
         $(".react-datepicker__year-select").selectOption("2020");
-        $$(".react-datepicker__day").find(Condition.text("12")).click();
+        $$(".react-datepicker__day").find(text("12")).click();
         $("#subjectsInput").setValue("Physics").pressEnter();
         $(byText("Music")).click();
         $(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("facemy.jpg");
+        $("#currentAddress").setValue("SomeAddress");
+        $("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
+        $("#react-select-4-input").setValue("Agra").pressEnter();
+        $("#submit").click();
+
+        $(".table-responsive").shouldHave(text("1Name 2LastName"), text("qwe@qas.ru"),
+                text("Female"), text("1234567891"), text("12 January,2020"),
+                text("Physics"), text("Music, Sports"), text("facemy.jpg"), text("SomeAddress"),
+                text("Uttar Pradesh Agra"));
+
     }
 }
